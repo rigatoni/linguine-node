81878defa419c2c9bc3a2320ca410265e4e09aaf
Keegan Parrotte <kmp3325@rit.edu>
2015-10-12 13:28:54 -0400

    Check that a corpus was uploaded and that a title was provided on creating a new one.

--- assets/js/corpora/new.controller.js
+++ assets/js/corpora/new.controller.js
@@ -4,7 +4,7 @@
     .module('linguine.corpora')
     .controller('CorporaNewController', CorporaNewController);
 
-  function CorporaNewController($scope, $upload, $state, $http) {
+  function CorporaNewController($scope, $upload, $state, $http, $rootScope, flash) {
 
     $scope.corpus = {};
     $scope.files = [];
@@ -16,20 +16,28 @@
     $scope.upload = function () {
       if ( $scope.files && $scope.files.length) {
         for (var i = 0; i < $scope.files.length; i++) {
-          var file = $scope.files[i];
-          $upload
-            .upload({
-              url: 'api/corpora',
-              data: $scope.corpus,
-              file: file,
-            })
-            .progress(function (evt) {
-              $scope.progress = parseInt( 100.0 * evt.loaded /evt. total );
-            })
-            .success(function(data){
-              $state.go('linguine.corpora.index');
-            })
+          if ($scope.corpus.title === undefined || $scope.corpus.title.replace(/ /g,'') === '') {
+            flash.danger.setMessage('Your corpus must have a title.');
+            $rootScope.$emit("event:angularFlash");
+          } else {
+            var file = $scope.files[i];
+            $upload
+                .upload({
+                  url: 'api/corpora',
+                  data: $scope.corpus,
+                  file: file,
+                })
+                .progress(function (evt) {
+                  $scope.progress = parseInt( 100.0 * evt.loaded /evt. total );
+                })
+                .success(function(data){
+                  $state.go('linguine.corpora.index');
+                })
+          }
         }
+      } else {
+        flash.danger.setMessage('You have not uploaded a corpus.');
+        $rootScope.$emit("event:angularFlash");
       }
     }
 
